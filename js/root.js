d246d638701e1fc66da0ac13b1eed748ccd18ba3
Katarzyna Wicherska <kawi@gft.com>
2017-03-16 12:07:34 +0100

    pictures, for testing, not sure if final heroes, can be changed on the way, moving js from html to js file

---
+++ js/root.js
@@ -0,0 +1,25 @@
+var type = "WebGL"
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas"
+}
+PIXI.utils.sayHello(type)
+
+var stage = new PIXI.Container(), //Create a container object - `stage`
+    renderer = PIXI.autoDetectRenderer(500, 500); //Creating renderer
+document.body.appendChild(renderer.view); //Adding canvas to the HTML document
+
+
+PIXI.loader
+    .add('images/Idle (1).png')
+    .load(setup) //Using built-in `loader` object to load images
+
+function setup() {
+    var zombie = new PIXI.Sprite(
+        PIXI.loader.resources['images/Idle (1).png'].texture
+    );
+
+
+    stage.addChild(zombie); //Adding zombie to the stages
+
+    renderer.render(stage); //Tell the `renderer` to `render` the `stage` 
+}
