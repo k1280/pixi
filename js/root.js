daef3e0bec9bc96cce3754b388dbc5d7f3a65319
Katarzyna Wicherska <kawi@gft.com>
2017-03-16 22:05:06 +0100

    setting up keyboard, still doesn't work, need to be investigated

--- js/root.js
+++ js/root.js
@@ -30,29 +30,123 @@ loader
 // function loadProgressHandler() {
 //     console.log("loading");
 // }
-
+var zombie;
 function setup() {
 
     // Create `zombie` sprite, add it to the stage, and render it
-    var zombie = new Sprite(resources['images/Idle (1).png'].texture);
+    zombie = new Sprite(resources['images/Idle (1).png'].texture);
 
     // new position for zombie - for now tossed in the air
     zombie.height = 120;
     zombie.width = 90;
 
     stage.addChild(zombie);
-    renderer.render(stage);
 
-    function gameLoop() {
-        // Loop this function at 60 frames per second
-        requestAnimationFrame(gameLoop);
-        // more like pushing the zombie forward
-        zombie.x += 1;
-        //Render the stage to see the animation
-        renderer.render(stage);
-    }
+    //     //Capture the keyboard arrow keys
+    //   var left = keyboard(37),
+    //       up = keyboard(38),
+    //       right = keyboard(39),
+    //       down = keyboard(40);
 
     //Start the game loop
     gameLoop();
+
+}
+function gameLoop() {
+    // Loop this function at 60 frames per second
+    requestAnimationFrame(gameLoop);
+    // more like pushing the zombie forward
+    zombie.x += 1;
+    //Render the stage to see the animation
+    renderer.render(stage);
 }
 
+
+// TODO: I was following tutorial step by step; 
+// TODO: investigate why it doesn't work?? -> error in browser console; how to set up gamepad??
+// function setup() {
+
+//     // Create the `zombie` sprite
+//     zombie = new Sprite("images/zombie.png");
+//     zombie.y = 96;
+//     zombie.vx = 0;
+//     zombie.vy = 0;
+//     stage.addChild(zombie);
+
+//     //Capture the keyboard arrow keys
+//     var left = keyboard(37),
+//         up = keyboard(38),
+//         right = keyboard(39),
+//         down = keyboard(40);
+
+//     //Left arrow key `press` method
+//     left.press = function () {
+
+//         //Change the zombie's velocity when the key is pressed
+//         zombie.vx = -5;
+//         zombie.vy = 0;
+//     };
+
+//     //Left arrow key `release` method
+//     left.release = function () {
+
+//         //If the left arrow has been released, and the right arrow isn't down,
+//         //and the zombie isn't moving vertically:
+//         //Stop the zombie
+//         if (!right.isDown && zombie.vy === 0) {
+//             zombie.vx = 0;
+//         }
+//     };
+
+//     //Up
+//     up.press = function () {
+//         zombie.vy = -5;
+//         zombie.vx = 0;
+//     };
+//     up.release = function () {
+//         if (!down.isDown && zombie.vx === 0) {
+//             zombie.vy = 0;
+//         }
+//     };
+
+//     //Right
+//     right.press = function () {
+//         zombie.vx = 5;
+//         zombie.vy = 0;
+//     };
+//     right.release = function () {
+//         if (!left.isDown && zombie.vy === 0) {
+//             zombie.vx = 0;
+//         }
+//     };
+
+//     //Down
+//     down.press = function () {
+//         zombie.vy = 5;
+//         zombie.vx = 0;
+//     };
+//     down.release = function () {
+//         if (!up.isDown && zombie.vx === 0) {
+//             zombie.vy = 0;
+//         }
+//     };
+
+//     //Set the game state
+//     state = play;
+
+//     //Start the game loop
+//     gameLoop();
+// }
+
+// function gameLoop() {
+//     requestAnimationFrame(gameLoop);
+//     state();
+//     renderer.render(stage);
+// }
+
+// function play() {
+
+//     //Use the zombie's velocity to make it move
+//     zombie.x += zombie.vx;
+//     zombie.y += zombie.vy
+// }
