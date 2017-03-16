0f54bc3810342ed837afa666d4639d0b90c6b536
Katarzyna Wicherska <kawi@gft.com>
2017-03-16 20:48:50 +0100

    code formatting, root js fix

--- js/root.js
+++ js/root.js
@@ -8,18 +8,15 @@ var stage = new PIXI.Container(), //Create a container object - `stage`
     renderer = PIXI.autoDetectRenderer(500, 500); //Creating renderer
 document.body.appendChild(renderer.view); //Adding canvas to the HTML document
 
-
-PIXI.loader
+PIXI.loader //Using built-in `loader` object to load images
     .add('images/Idle (1).png')
-    .load(setup) //Using built-in `loader` object to load images
+    .load(setup); 
 
 function setup() {
     var zombie = new PIXI.Sprite(
         PIXI.loader.resources['images/Idle (1).png'].texture
     );
 
-
     stage.addChild(zombie); //Adding zombie to the stages
-
     renderer.render(stage); //Tell the `renderer` to `render` the `stage` 
 }
